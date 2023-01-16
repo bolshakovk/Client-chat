3b6133f1b84f5cd45e701946894b047544a23581
bolsh <bolsh@DESKTOP-I3AE3F2>
2023-01-16 10:12:04 +0300

    Добавьте файлы проекта.

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterChat = new System.Windows.Forms.Button();
+            this.gue_userName = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.chatBox = new System.Windows.Forms.TextBox();
+            this.chat_send = new System.Windows.Forms.Button();
+            this.chat_msg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterChat
+            // 
+            this.enterChat.Location = new System.Drawing.Point(682, 36);
+            this.enterChat.Name = "enterChat";
+            this.enterChat.Size = new System.Drawing.Size(75, 23);
+            this.enterChat.TabIndex = 0;
+            this.enterChat.Text = "Вход";
+            this.enterChat.UseVisualStyleBackColor = true;
+            this.enterChat.Click += new System.EventHandler(this.enterChat_Click);
+            // 
+            // gue_userName
+            // 
+            this.gue_userName.AutoSize = true;
+            this.gue_userName.Location = new System.Drawing.Point(498, 36);
+            this.gue_userName.Name = "gue_userName";
+            this.gue_userName.Size = new System.Drawing.Size(72, 13);
+            this.gue_userName.TabIndex = 1;
+            this.gue_userName.Text = "Введите имя";
+            // 
+            // userName
+            // 
+            this.userName.Location = new System.Drawing.Point(576, 36);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(100, 20);
+            this.userName.TabIndex = 2;
+            // 
+            // chatBox
+            // 
+            this.chatBox.Enabled = false;
+            this.chatBox.Location = new System.Drawing.Point(12, 130);
+            this.chatBox.Multiline = true;
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(745, 260);
+            this.chatBox.TabIndex = 3;
+            this.chatBox.TextChanged += new System.EventHandler(this.chatBox_TextChanged);
+            // 
+            // chat_send
+            // 
+            this.chat_send.Enabled = false;
+            this.chat_send.Location = new System.Drawing.Point(682, 396);
+            this.chat_send.Name = "chat_send";
+            this.chat_send.Size = new System.Drawing.Size(75, 23);
+            this.chat_send.TabIndex = 4;
+            this.chat_send.Text = "Отправить";
+            this.chat_send.UseVisualStyleBackColor = true;
+            this.chat_send.Click += new System.EventHandler(this.chat_send_Click);
+            // 
+            // chat_msg
+            // 
+            this.chat_msg.Enabled = false;
+            this.chat_msg.Location = new System.Drawing.Point(13, 397);
+            this.chat_msg.Name = "chat_msg";
+            this.chat_msg.Size = new System.Drawing.Size(663, 20);
+            this.chat_msg.TabIndex = 5;
+            this.chat_msg.TextChanged += new System.EventHandler(this.chat_msg_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(13, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Чат";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chat_msg);
+            this.Controls.Add(this.chat_send);
+            this.Controls.Add(this.chatBox);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.gue_userName);
+            this.Controls.Add(this.enterChat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enterChat;
+        private System.Windows.Forms.Label gue_userName;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.TextBox chatBox;
+        private System.Windows.Forms.Button chat_send;
+        private System.Windows.Forms.TextBox chat_msg;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        // Делегаты 
+        private delegate void printer(string data);
+        private delegate void cleaner();
+
+        readonly printer Printer;
+        readonly cleaner Cleaner;
+        private Socket _serverSocket;
+        private Thread _clientThread;
+        private const string _serverHost = "localhost";
+        private const int _serverPort = 9933;
+        public Form1()
+        {
+            InitializeComponent();
+            Printer = new printer(print);
+            Cleaner = new cleaner(ClearChat);
+            Connect();
+            _clientThread = new Thread(Listner)
+            {
+                IsBackground = true
+            };
+            _clientThread.Start();
+        }
+        // Считывание даты с сервера
+        private void Listner()
+        {
+            while (_serverSocket.Connected)
+            {
+                byte[] buffer = new byte[8196];
+                int bytesRec = _serverSocket.Receive(buffer);
+                string data = Encoding.UTF8.GetString(buffer, 0, bytesRec);
+                if (data.Contains("#updatechat"))
+                {
+                    UpdateChat(data);
+                    continue;
+                }
+            }
+        }
+        // Коннект к серверу
+        private void Connect()
+        {
+            try
+            {
+                IPHostEntry ipHost = Dns.GetHostEntry(_serverHost);
+                IPAddress ipAddress = ipHost.AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, _serverPort);
+                _serverSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                _serverSocket.Connect(ipEndPoint);
+            }
+            catch { print("Сервер недоступен!"); }
+        }
+        // Отчистка текстбокса
+        private void ClearChat()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(Cleaner);
+                return;
+            }
+            chatBox.Clear();
+        }
+        // Отрисовка сообщений в чате
+        private void UpdateChat(string data)
+        {
+            //#updatechat&userName~data|username~data
+            ClearChat();
+            string[] messages = data.Split('&')[1].Split('|');
+            int countMessages = messages.Length;
+            if (countMessages <= 0) return;
+            for (int i = 0; i < countMessages; i++)
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(messages[i])) continue;
+                    print(String.Format("[{0}]:{1}.", messages[i].Split('~')[0], messages[i].Split('~')[1]));
+                }
+                catch { continue; }
+            }
+        }
+        // Передача побайтовой сообщения на сервер
+        private void Send(string data)
+        {
+            try
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(data);
+                int bytesSent = _serverSocket.Send(buffer);
+            }
+            catch { print("Связь с сервером прервалась..."); }
+        }
+        // отрисовка в текстбоксе сообщения
+        private void print(string msg)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(Printer, msg);
+                return;
+            }
+            if (chatBox.Text.Length == 0)
+                chatBox.AppendText(msg);
+            else
+                chatBox.AppendText(Environment.NewLine + msg);
+        }
+
+        // Отправка сообщения
+        private void sendMessage()
+        {
+            try
+            {
+                string data = chat_msg.Text;
+                if (string.IsNullOrEmpty(data)) return;
+                Send("#newmsg&" + data);
+                chat_msg.Text = string.Empty;
+            }
+            catch { MessageBox.Show("Ошибка при отправке сообщения!"); }
+        }
+        private void chatBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+                sendMessage();
+        }
+
+        // Отправка сообщения по клику на кнопку
+        private void chat_send_Click(object sender, EventArgs e)
+        {
+            sendMessage();
+        }
+        // Кнопка войти в чат, позволяет печатать в чат, отправляет имя пользователя на сервер через сокет
+        private void enterChat_Click(object sender, EventArgs e)
+        {
+            string Name = userName.Text;
+            if (string.IsNullOrEmpty(Name)) return;
+            Send("#setname&" + Name);
+            chat_msg.Enabled = true;
+            chat_send.Enabled = true;
+            userName.Enabled = false;
+            enterChat.Enabled = false;
+        }
+
+
+        private void chat_msg_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chatBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
